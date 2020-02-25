90c522510e65403c9e3181f67bbefa8ecc68d365
baandr <bandrews@physik.uzh.ch>
2020-02-25 11:04:33 -0800

    added stdout directory

--- data/create_directories.sh
+++ data/create_directories.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-for tool in charge_pump corr_len_kappa_flow double_occ ent_spec_flow ent_spec_mom ent_spec_V_flow corr_len corr_len_tau_flow energy ent_spec_invt2dash_flow ent_spec_real overlap corr_len_invt2dash_flow ent_scal ent_spec_kappa_flow ent_spec_tau_flow
+#for tool in charge_pump corr_len_kappa_flow double_occ ent_spec_flow ent_spec_mom ent_spec_V_flow corr_len corr_len_tau_flow energy ent_spec_invt2dash_flow ent_spec_real overlap corr_len_invt2dash_flow ent_scal ent_spec_kappa_flow ent_spec_tau_flow
+for tool in phi_flow Ly_flow kappa_flow
 do
     for model in BosonicHofstadter FermionicHofstadter BosonicHex1 FermionicHex1 BosonicHex1Hex5 FermionicHex1Hex5 BosonicHex1Hex5Orbital FermionicHex1Hex5Orbital 
     do
         #mkdir -p $tool/$model
-        printf '%s\n%s' '*' '!.gitignore' > $tool/$model/.gitignore
+        printf '%s\n%s' '*' '!.gitignore' > stdout/$tool/$model/.gitignore
     done
 done
