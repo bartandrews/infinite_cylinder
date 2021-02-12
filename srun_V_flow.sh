e9206a2efb93926d34a09941c8db936d5dd3d3bd
baandr <bandrews@physik.uzh.ch>
2021-02-12 18:05:00 +0100

    update sjobs files

--- srun_V_flow.sh
+++ srun_V_flow.sh
@@ -18,8 +18,8 @@ for FILE_PATH in $(ls $SJOBS_FILES | sort -h -t '_' -k12,12n)  # sort by q
 do
 	FILE="$(basename "$FILE_PATH")"  # strip the file from the path
 	sbatch --output=$SLOGS/"$FILE".out --error=$SLOGS/"$FILE".err --job-name="$FILE" --mail-type=ALL \
-	--mail-user=bandrews@physik.uzh.ch --qos=long --time=168:00:00 --mem=64G --ntasks=1 \
-	--cpus-per-task=16 --threads-per-core=1 "$FILE_PATH"
+	--mail-user=bandrews@physik.uzh.ch --qos=long --time=168:00:00 --mem=185G --ntasks=1 \
+	--cpus-per-task=12 --threads-per-core=1 "$FILE_PATH"
 done
 
 SLOGS="/home/cluster/baandr/infinite_cylinder/slogs"
@@ -29,8 +29,8 @@ for FILE_PATH in $(ls $SJOBS_FILES | sort -h -t '_' -k12,12n)  # sort by q
 do
 	FILE="$(basename "$FILE_PATH")"  # strip the file from the path
 	sbatch --output=$SLOGS/"$FILE".out --error=$SLOGS/"$FILE".err --job-name="$FILE" --mail-type=ALL \
-	--mail-user=bandrews@physik.uzh.ch --qos=long --time=168:00:00 --mem=64G --ntasks=1 \
-	--cpus-per-task=16 --threads-per-core=1 "$FILE_PATH"
+	--mail-user=bandrews@physik.uzh.ch --qos=long --time=168:00:00 --mem=185G --ntasks=1 \
+	--cpus-per-task=12 --threads-per-core=1 "$FILE_PATH"
 done
 
 SLOGS="/home/cluster/baandr/infinite_cylinder/slogs"
@@ -40,8 +40,8 @@ for FILE_PATH in $(ls $SJOBS_FILES | sort -h -t '_' -k12,12n)  # sort by q
 do
 	FILE="$(basename "$FILE_PATH")"  # strip the file from the path
 	sbatch --output=$SLOGS/"$FILE".out --error=$SLOGS/"$FILE".err --job-name="$FILE" --mail-type=ALL \
-	--mail-user=bandrews@physik.uzh.ch --qos=long --time=168:00:00 --mem=64G --ntasks=1 \
-	--cpus-per-task=16 --threads-per-core=1 "$FILE_PATH"
+	--mail-user=bandrews@physik.uzh.ch --qos=long --time=168:00:00 --mem=185G --ntasks=1 \
+	--cpus-per-task=12 --threads-per-core=1 "$FILE_PATH"
 done
 
 SLOGS="/home/cluster/baandr/infinite_cylinder/slogs"
@@ -51,8 +51,8 @@ for FILE_PATH in $(ls $SJOBS_FILES | sort -h -t '_' -k12,12n)  # sort by q
 do
 	FILE="$(basename "$FILE_PATH")"  # strip the file from the path
 	sbatch --output=$SLOGS/"$FILE".out --error=$SLOGS/"$FILE".err --job-name="$FILE" --mail-type=ALL \
-	--mail-user=bandrews@physik.uzh.ch --qos=long --time=168:00:00 --mem=64G --ntasks=1 \
-	--cpus-per-task=16 --threads-per-core=1 "$FILE_PATH"
+	--mail-user=bandrews@physik.uzh.ch --qos=long --time=168:00:00 --mem=185G --ntasks=1 \
+	--cpus-per-task=12 --threads-per-core=1 "$FILE_PATH"
 done
 
 SLOGS="/home/cluster/baandr/infinite_cylinder/slogs"
@@ -62,8 +62,8 @@ for FILE_PATH in $(ls $SJOBS_FILES | sort -h -t '_' -k12,12n)  # sort by q
 do
 	FILE="$(basename "$FILE_PATH")"  # strip the file from the path
 	sbatch --output=$SLOGS/"$FILE".out --error=$SLOGS/"$FILE".err --job-name="$FILE" --mail-type=ALL \
-	--mail-user=bandrews@physik.uzh.ch --qos=long --time=168:00:00 --mem=64G --ntasks=1 \
-	--cpus-per-task=16 --threads-per-core=1 "$FILE_PATH"
+	--mail-user=bandrews@physik.uzh.ch --qos=long --time=168:00:00 --mem=185G --ntasks=1 \
+	--cpus-per-task=12 --threads-per-core=1 "$FILE_PATH"
 done
 
 SLOGS="/home/cluster/baandr/infinite_cylinder/slogs"
@@ -73,6 +73,6 @@ for FILE_PATH in $(ls $SJOBS_FILES | sort -h -t '_' -k12,12n)  # sort by q
 do
 	FILE="$(basename "$FILE_PATH")"  # strip the file from the path
 	sbatch --output=$SLOGS/"$FILE".out --error=$SLOGS/"$FILE".err --job-name="$FILE" --mail-type=ALL \
-	--mail-user=bandrews@physik.uzh.ch --qos=long --time=168:00:00 --mem=64G --ntasks=1 \
-	--cpus-per-task=16 --threads-per-core=1 "$FILE_PATH"
+	--mail-user=bandrews@physik.uzh.ch --qos=long --time=168:00:00 --mem=185G --ntasks=1 \
+	--cpus-per-task=12 --threads-per-core=1 "$FILE_PATH"
 done
